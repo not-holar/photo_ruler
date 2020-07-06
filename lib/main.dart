cfedd4d36f2663eeb65608771297898cbc0d06ae
Holar <vitalii.hurianov@gmail.com>
2020-07-06 17:13:04 +0300

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:filepicker_windows/filepicker_windows.dart';
-import 'package:flutter/gestures.dart';
+// import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
@@ -31,8 +31,8 @@ class App extends StatelessWidget {
         ChangeNotifierProvider<ValueNotifier<List<RulerArrow>>>(
           create: (_) => ValueNotifier([
             RulerArrow(
-              const Point(.2, .4),
-              const Point(.6, .5),
+              const Point(20, 4),
+              const Point(600, 500),
             )
           ]),
         ),
@@ -121,116 +121,6 @@ class Home extends StatelessWidget {
   }
 }
 
-// class Home extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final _controller = PhotoViewController();
-//     final _globalKey = GlobalKey();
-
-//     return Scaffold(
-//       backgroundColor: Theme.of(context).backgroundColor,
-//       body: Consumer<ValueNotifier<Photo>>(
-//         builder: (context, photo, _) {
-//           if (photo.value == null) {
-//             return Center(
-//               child: RaisedButton.icon(
-//                 onPressed: () async {
-//                   final picker = FilePicker();
-//                   picker.title = 'Select an image';
-//                   picker.filterSpecification = {'All Files (*.*)': '*.*'};
-
-//                   final file = picker.getFile();
-
-//                   if (file == null) return;
-
-//                   photo.value = await Photo.fromList(
-//                     await file.readAsBytes(),
-//                   );
-//                 },
-//                 padding: const EdgeInsets.symmetric(
-//                   horizontal: 32,
-//                   vertical: 20,
-//                 ),
-//                 icon: const Icon(Icons.photo),
-//                 label: const Text(
-//                   "Open Image",
-//                   textScaleFactor: 1.2,
-//                 ),
-//               ),
-//             );
-//           }
-
-//           return LayoutBuilder(builder: (context, constraints) {
-//             final screenSize = Offset(
-//               constraints.maxWidth,
-//               constraints.maxHeight,
-//             );
-//             Offset mousePosition = Offset.zero;
-
-//             return MouseRegion(
-//               onHover: (event) {
-//                 mousePosition = event.position;
-//               },
-//               child: Listener(
-//                 onPointerSignal: (pointerSignal) async {
-//                   if (pointerSignal is PointerScrollEvent) {
-//                     final sign = pointerSignal.scrollDelta.dy.sign;
-//                     final divisor = 1 + sign / 10;
-//                     final newScale = _controller.scale / divisor;
-//                     final cursorPosition = -(mousePosition - screenSize / 2);
-//                     final newPosition =
-//                         (_controller.position + cursorPosition) / divisor -
-//                             cursorPosition;
-
-//                     _controller.updateMultiple(
-//                       scale: newScale,
-//                       position: newPosition,
-//                     );
-//                   }
-//                 },
-//                 child: PhotoView.customChild(
-//                   key: _globalKey,
-//                   childSize: photo.value.size,
-//                   backgroundDecoration: const BoxDecoration(
-//                     color: Colors.transparent,
-//                   ),
-//                   controller: _controller,
-//                   // childSize: image.value.item2,
-//                   child: Center(
-//                     child: AspectRatio(
-//                       aspectRatio: photo.value.size.aspectRatio,
-//                       child: Material(
-//                         color: Colors.transparent,
-//                         borderRadius: BorderRadius.zero,
-//                         elevation: 12,
-//                         child: Consumer2<ValueNotifier<double>,
-//                             ValueNotifier<List<RulerArrow>>>(
-//                           builder: (context, scale, arrows, _) {
-//                             return CustomPaint(
-//                               size: photo.value.size,
-//                               isComplex: true,
-//                               willChange: true,
-//                               painter: PhotoPainter(
-//                                 photo.value,
-//                                 arrows.value,
-//                                 scale.value,
-//                               ),
-//                             );
-//                           },
-//                         ),
-//                       ),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             );
-//           });
-//         },
-//       ),
-//     );
-//   }
-// }
-
 class RulerEditor extends StatefulWidget {
   final Photo photo;
 
@@ -252,6 +142,8 @@ class _RulerEditorState extends State<RulerEditor> {
       ),
       Expanded(
         child: Builder(builder: (context) {
+          final scale = context.watch<ValueNotifier<double>>().value;
+
           return Stack(
             children: context
                 .watch<ValueNotifier<List<RulerArrow>>>()
@@ -259,100 +151,66 @@ class _RulerEditorState extends State<RulerEditor> {
                 .asMap()
                 .entries
                 .map(
-                  arrowBuilder,
+                  (arrow) => arrowBuilder(arrow.key, arrow.value, scale),
                 )
                 .toList(),
           );
         }),
       ),
     ]);
-
-    // return CustomPaint(
-    //   isComplex: true,
-    //   willChange: true,
-    //   painter: PhotoPainter(
-    //     photo: widget.photo,
-    //     arrows: Provider.of<ValueNotifier<List<RulerArrow>>>(
-    //       context,
-    //       listen: false,
-    //     ),
-    //     scale: Provider.of<ValueNotifier<double>>(
-    //       context,
-    //       listen: false,
-    //     ),
-    //   ),
-    // );
   }
 
-  Widget arrowBuilder(MapEntry<int, RulerArrow> arrow) {
-    final startOffset = Offset(
-      arrow.value.start.x,
-      arrow.value.start.y,
-    );
-    final endOffset = Offset(
-      arrow.value.end.x,
-      arrow.value.end.y,
-    );
-
-    final rect = Rect.fromPoints(
-      startOffset,
-      endOffset,
-    );
-
-    return FractionallySizedBox(
-      child: AnimatedAlign(
-        key: ValueKey(arrow.key),
-        alignment: Alignment(rect.left, rect.top),
-        duration: const Duration(milliseconds: 250),
-        child: const ColoredBox(
-          color: Colors.pinkAccent,
-          child: SizedBox.expand(),
-        ),
+  Widget arrowBuilder(int index, RulerArrow arrow, double scale) {
+    return CustomPaint(
+      key: ValueKey(index),
+      painter: ArrowPainter(
+        arrow: arrow,
+        scale: scale,
+        imageSize: widget.photo.size,
       ),
     );
   }
 }
 
-class PhotoPainter extends CustomPainter {
-  final Photo photo;
-  final ValueNotifier<List<RulerArrow>> arrows;
-  final ValueNotifier<double> scale;
+class ArrowPainter extends CustomPainter {
+  final RulerArrow arrow;
+  final double scale;
+  final Size imageSize;
 
-  PhotoPainter({
-    @required this.photo,
-    @required this.arrows,
+  ArrowPainter({
+    @required this.arrow,
     @required this.scale,
-  })  : assert(photo != null),
-        assert(arrows != null),
+    @required this.imageSize,
+  })  : assert(arrow != null),
         assert(scale != null),
-        super(repaint: Listenable.merge([arrows, scale]));
+        assert(imageSize != null);
+
+  final arrowPaint = Paint()
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 4.0
+    ..color = Colors.indigo;
 
   @override
   void paint(Canvas canvas, Size size) {
-    print(size);
+    final renderScale = imageSize.width / size.width;
 
-    paintImage(
-      canvas: canvas,
-      rect: Rect.fromLTWH(0, 0, size.width, size.height),
-      image: photo.image,
-      filterQuality: FilterQuality.high,
+    canvas.drawPath(
+      Path()
+        ..moveTo(
+          arrow.start.x * renderScale,
+          arrow.start.y * renderScale,
+        )
+        ..lineTo(
+          arrow.end.x * renderScale,
+          arrow.end.y * renderScale,
+        ),
+      arrowPaint,
     );
-
-    final arrowPaint = Paint()
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 4.0
-      ..color = Colors.indigo;
-
-    for (final arrow in arrows.value) {
-      canvas.drawPath(
-        Path()
-          ..moveTo(arrow.start.x, arrow.start.y)
-          ..lineTo(arrow.end.x, arrow.end.y),
-        arrowPaint,
-      );
-    }
   }
 
   @override
-  bool shouldRepaint(PhotoPainter oldDelegate) => true;
+  bool shouldRepaint(ArrowPainter oldDelegate) =>
+      arrow.start != oldDelegate.arrow.start ||
+      arrow.end != oldDelegate.arrow.end ||
+      scale != oldDelegate.scale;
 }
